ecb6709ddd70e6d20cd1df09480df8d69920585f
Yannis Stergiou <stergiou1234567890@gmail.com>
2026-08-12 22:58:56 +0300

    type fixed

--- pdf2markdown.py
+++ pdf2markdown.py
@@ -277,7 +277,7 @@ def api_picker():
 
         if env_api_key:
             picked_api["api_key"] = env_api_key
-            print(f"{picked_api_name}API key leaded from enviroment\n")
+            print(f"{picked_api_name}API key loaded from enviroment\n")
         else:
             user_input_api_key    = input(f"Please paste you {picked_api['name']} API key\nNOTE: If API key is wrong, the program is going to crash\n")
 
